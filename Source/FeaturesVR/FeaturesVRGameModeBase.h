85c4098589099c402ecb5b4c13f1b831e1bf8e98
NekiCoule <webermax.mail@gmail.com>
2019-05-07 15:01:12 +0200

    Initial commit

---
+++ Source/FeaturesVR/FeaturesVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FeaturesVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEATURESVR_API AFeaturesVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
