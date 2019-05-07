b1d027ac3504f66c19d2e5feb7e2178b34e6138d
NekiCoule <webermax.mail@gmail.com>
2019-05-07 16:06:47 +0200

    Add movement blueprint

---
+++ Source/FeaturesVR/GameModeBaseVR.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FeaturesVRGameModeBase.h"
+#include "GameModeBaseVR.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEATURESVR_API AGameModeBaseVR : public AFeaturesVRGameModeBase
+{
+	GENERATED_BODY()
+	
+};
